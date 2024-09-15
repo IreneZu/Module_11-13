bd5093ce7ff33a5c881b3502980b27b7e7a26992
IreneZu <izuraeva@gmail.com>
2024-09-15 15:24:28 +0300

    Update module_11_2.py

--- module_11_2.py
+++ module_11_2.py
@@ -17,15 +17,37 @@ class Eagle():  # Орёл
 def introspection_info(obj):
     chars = {}
     chars['type'] = type(obj)
-    chars['attributes'] = list(vars(obj).keys())
-    chars['methods'] = inspect.getmembers(obj, predicate=inspect.ismethod)
-    chars['module'] = __name__ #inspect.getmodulename(obj)
-    chars['file'] = path.basename(inspect.getfile(obj.fly))
+    chars['attributes'] = [i for i in dir(obj) if not callable(getattr(obj,i))]
+    chars['methods_all'] = [i for i in dir(obj) if callable(getattr(obj,i))]
+    chars['methods_of_object'] = [i[0] for i in inspect.getmembers(obj, predicate=inspect.ismethod)]
+
+    chars['module'] = obj.__module__ if hasattr(obj, '__module__') else obj.__class__.__module__
+
+    try:
+        chars['file'] = path.basename(inspect.getfile(obj))
+    except TypeError:
+        try:
+            chars['file'] = path.basename(inspect.getfile(type(obj)))
+        except:
+            chars['file'] = obj.__class__.__module__
 
     return chars
 
+print('Object of class Eagle\n---------------------------------------------------------------')
 eagle_obj = Eagle()
 pprint(introspection_info(eagle_obj))
+
+# print('\nFunction introspection_info\n---------------------------------------------------------------')
+# func_obj = introspection_info
+# pprint(introspection_info(func_obj))
+
+print('\nFunction inspect.ismethod\n---------------------------------------------------------------')
+func_obj = inspect.ismethod
+pprint(introspection_info(func_obj))
+
+print('\nNumber 42\n---------------------------------------------------------------')
+number_obj = 42
+pprint(introspection_info(number_obj))
 
 
 
